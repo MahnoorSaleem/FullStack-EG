0575e14f051b692bbe41cf80e0efdb8151b8f508
MahnoorSaleem <mhnrsaleem579@gmail.com>
2024-08-03 23:57:53 +0400

    Create User Password Validation Updated

--- BE/src/user/dto/create-user.dto.ts
+++ BE/src/user/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, IsStrongPassword, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
     @IsString()
@@ -12,6 +12,14 @@ export class CreateUserDto {
 
     @IsString()
     @IsNotEmpty()
+    @MinLength(4)
     @MaxLength(20)
+    @IsStrongPassword({
+        minLength: 8,
+        minLowercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+        minUppercase: 1
+    })
     password: string;
 }
