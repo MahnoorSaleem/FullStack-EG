1e3d481c0722b1e3dab048cb6286f461fa613815
MahnoorSaleem <mhnrsaleem579@gmail.com>
2024-08-03 23:42:07 +0400

    security steps

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -4,9 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }]),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_CONNECTION), 
     AuthModule,
--- BE/src/main.ts
+++ BE/src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.enableCors();
-  // app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(csurf());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const port = process.env.PORT || 3001
   await app.listen(port);
